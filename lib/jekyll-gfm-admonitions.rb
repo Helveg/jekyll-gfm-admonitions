73edbe7e1ba83396c0dd81576e2bb4e3e7ba15c8
Robin De Schepper <robin.deschepper93@gmail.com>
2024-09-28 13:23:41 -0400

    dropped patches, linted

--- lib/jekyll-gfm-admonitions.rb
+++ lib/jekyll-gfm-admonitions.rb
@@ -24,56 +24,54 @@ module JekyllGFMAdmonitions
   class GFMAdmonitionConverter < Jekyll::Generator
     safe true
     priority :lowest
-    @@admonition_pages = []
-
-    def initialize(*args)
-      super(*args)
-      @converted = 0
-    end
+    @admonition_pages = []
 
     def generate(site)
-      @markdown = site.converters.find { |c| c.is_a?(Jekyll::Converters::Markdown) }
-      unless @markdown
-        raise "Markdown converter not found. Please ensure that you have a markdown converter configured in your Jekyll site."
-      end
+      init_converter(site)
+      process_posts(site)
+      process_pages(site)
+      Jekyll.logger.info 'GFMA:', 'Converted adminitions in' \
+        " #{self.class.admonition_pages.length} file(s)."
+    end
 
-      # Process admonitions in posts
+    def init_converter(site)
+      @markdown = site.converters.find { |c| c.is_a?(Jekyll::Converters::Markdown) }
+      return if @markdown
+
+      raise 'Markdown converter not found. Please ensure that you have a markdown' \
+              ' converter configured in your Jekyll site.'
+    end
+
+    def process_posts(site)
       site.posts.docs.each do |doc|
         Jekyll.logger.debug 'GFMA:', "Processing post '#{doc.path}' (#{doc.content.length} characters)."
-        process(doc)
+        process_doc(doc)
       end
-
-      # Process admonitions in pages
-      site.pages.each do |page|
-        # # Patch the root README for GitHub Pages builds
-        # if page.path == 'README.md' && page.dir == '/'
-        #   Jekyll.logger.info 'GFMA:', "Patched /README.html to /index.html"
-        #   page.instance_variable_set(:@url, '/index.html')
-        # end
-
-        Jekyll.logger.debug 'GFMA:', "Processing page '#{page.path}' (#{page.content.length} characters)."
-        process(page)
-      end
-
-      Jekyll.logger.info 'GFMA:', "Converted adminitions in #{@converted} file(s)."
     end
 
-    def process(doc)
+    def process_pages(site)
+      site.pages.each do |page|
+        Jekyll.logger.debug 'GFMA:', "Processing page '#{page.path}' (#{page.content.length} characters)."
+        process_doc_content(page)
+      end
+    end
+
+    def process_doc_content(doc)
       original_content = doc.content.dup
-      convert_admonitions(doc)
+      process_doc(doc)
 
       return unless doc.content != original_content
+
       # Store a reference to all the pages we modified, to inject the CSS post render
       # (otherwise GitHub Pages sanitizes the CSS into plaintext)
-      @@admonition_pages << doc
-      @converted += 1
+      self.class.admonition_pages << doc
     end
 
-    def self.admonition_pages
-      return @@admonition_pages
+    class << self
+      attr_reader :admonition_pages
     end
 
-    def convert_admonitions(doc)
+    def process_doc(doc)
       code_blocks = []
       # Temporarily replace code blocks by a tag, so that we don't process any admonitions
       # inside of code blocks.
@@ -82,7 +80,15 @@ module JekyllGFMAdmonitions
         "```{{CODE_BLOCK_#{code_blocks.length - 1}}}```"
       end
 
-      # Match the admonition syntax
+      convert_admonitions(doc)
+
+      # Put the code blocks back in place
+      doc.content.gsub!(/```\{\{CODE_BLOCK_(\d+)}}```/) do
+        code_blocks[::Regexp.last_match(1).to_i]
+      end
+    end
+
+    def convert_admonitions(doc)
       doc.content.gsub!(/>\s*\[!(IMPORTANT|NOTE|WARNING|TIP|CAUTION)\]\s*\n((?:>.*\n?)*)/) do
         type = ::Regexp.last_match(1).downcase
         title = type.capitalize
@@ -90,17 +96,15 @@ module JekyllGFMAdmonitions
         icon = Octicons::Octicon.new(ADMONITION_ICONS[type]).to_svg
         Jekyll.logger.debug 'GFMA:', "Converting #{type} admonition."
 
-        # Replace them by the GFM admonition HTML
-        "<div class='markdown-alert markdown-alert-#{type}'>
+        admonition_html(type, title, text, icon)
+      end
+    end
+
+    def admonition_html(type, title, text, icon)
+      "<div class='markdown-alert markdown-alert-#{type}'>
           <p class='markdown-alert-title'>#{icon} #{title}</p>
           <p>#{@markdown.convert(text)}</p>
         </div>\n\n"
-      end
-
-      # Put the code blocks back in place
-      doc.content.gsub!(/```\{\{CODE_BLOCK_(\d+)}}```/) do
-        code_blocks[$1.to_i]
-      end
     end
   end
 
@@ -112,8 +116,8 @@ module JekyllGFMAdmonitions
       Jekyll.logger.debug 'GFMA:', "Appending admonition style to '#{page.path}'."
       css = File.read(File.expand_path('../assets/admonitions.css', __dir__))
 
-      page.output.gsub!(/<head>(.*?)<\/head>/m) do |match|
-        "#{match[0..-7]}<style>#{CSSminify.compress(css)}</style>#{match[-7..-1]}"
+      page.output.gsub!(%r{<head>(.*?)</head>}m) do |match|
+        "#{match[0..-7]}<style>#{CSSminify.compress(css)}</style>#{match[-7..]}"
       end
     end
   end
