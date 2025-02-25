Helveg/jekyll-gfm-admonitions

commit 0cba248e5cbb57d649e2dca893af5c7c8900400e

v1.1.0 - custom titles

lib/jekyll-gfm-admonitions.rb
@@ -90,10 +90,14 @@ module JekyllGFMAdmonitions
     end
 
     def convert_admonitions(doc)
-      doc.content.gsub!(/>\s*\[!(IMPORTANT|NOTE|WARNING|TIP|CAUTION)\]\s*\n((?:>.*\n?)*)/) do
+      doc.content.gsub!(/>\s*\[!(IMPORTANT|NOTE|WARNING|TIP|CAUTION)\]\s*(.*)\s*\n((?:>.*\n?)*)/) do
         type = ::Regexp.last_match(1).downcase
-        title = type.capitalize
-        text = ::Regexp.last_match(2).gsub(/^>\s*/, '').strip
+        if ::Regexp.last_match(2).length > 0
+          title = ::Regexp.last_match(2)
+        else
+          title = type.capitalize
+        end
+        text = ::Regexp.last_match(3).gsub(/^>\s*/, '').strip
         icon = Octicons::Octicon.new(ADMONITION_ICONS[type]).to_svg
         Jekyll.logger.debug 'GFMA:', "Converting #{type} admonition."
 

jekyll-gfm-admonitions.gemspec
@@ -4,7 +4,7 @@
 
 Gem::Specification.new do |spec|
   spec.name          = 'jekyll-gfm-admonitions'
-  spec.version       = '1.0.4'
+  spec.version       = '1.1.0'
   spec.authors       = ['Robin De Schepper']
   spec.email         = ['robin.deschepper93@gmail.com']
 

sample-github-pages/index.markdown
@@ -7,9 +7,14 @@ layout: home
 
 # GitHub Flavored Admonitions
 
+Use GitHub Flavored Admonitions on your GitHub Pages or Jekyll site. Admonition titles are supported.
 
 > [!NOTE]
 > I am a **note** admonition, useful to let interested users take note of something.
+
+
+> [!TIP] Titles are supported
+> You can add custom titles to your admonitions.
 
 
 > [!TIP]

sample/index.markdown
@@ -7,9 +7,14 @@ layout: home
 
 # GitHub Flavored Admonitions
 
+Use GitHub Flavored Admonitions on your GitHub Pages or Jekyll site. Admonition titles are supported.
 
 > [!NOTE]
 > I am a **note** admonition, useful to let interested users take note of something.
+
+
+> [!TIP] Titles are supported
+> You can add custom titles to your admonitions.
 
 
 > [!TIP]

commit c1955415d8fcc759a2e244fa518e6c94cb8d9443

Handle empty titles
0cba248e5cbb57d649e2dca893af5c7c8900400e introduced a bug when rendering titles.
First, it doesn't handle empty titles well. Second, the regex was too aggressive. It needs to stop at the newline.

This fixes that.

lib/jekyll-gfm-admonitions.rb
@@ -96,10 +96,10 @@ module JekyllGFMAdmonitions
     end
 
     def convert_admonitions(doc)
-      doc.content.gsub!(/>\s*\[!(IMPORTANT|NOTE|WARNING|TIP|CAUTION)\]\s*(.*)\s*\n((?:>.*\n?)*)/) do
+      doc.content.gsub!(/>\s*\[!(IMPORTANT|NOTE|WARNING|TIP|CAUTION)\]([^\n]*)\n((?:>.*\n?)*)/) do
         type = ::Regexp.last_match(1).downcase
-        if ::Regexp.last_match(2).length > 0
-          title = ::Regexp.last_match(2)
+        if ::Regexp.last_match(2).strip.length > 0
+          title = ::Regexp.last_match(2).strip
         else
           title = type.capitalize
         end